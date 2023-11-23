f9c5a45a065a189bed8485bbb1004ef2c5aa0afd
damiro <hello.damiro@gmail.com>
2023-11-24 02:32:13 +0800

    electron builder

---
+++ electron/electron.cjs
@@ -0,0 +1,44 @@
+const path = require('path');
+const { app, BrowserWindow } = require('electron');
+
+const isDev = process.env.IS_DEV == 'true' ? true : false;
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 1920,
+    height: 1080,
+    autoHideMenuBar: true,
+    resizable: true,
+    frame: true,
+    icon: path.join(__dirname, 'icon_64.png'),
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.cjs'),
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  mainWindow.webContents.setWindowOpenHandler((edata) => {
+    shell.openExternal(edata.url);
+    return { action: 'deny' };
+  });
+
+  mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../dist/index.html')}`);
+  // Open the DevTools.
+  if (isDev) {
+    //mainWindow.webContents.openDevTools();
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
